8fe0798e0253000680a5349550b60356e197c701
DWUNILU <149903811+DWUNILU@users.noreply.github.com>
2024-03-23 21:13:30 +0100

    Update Project-Depression.R

--- Project-Depression.R
+++ Project-Depression.R
@@ -5,27 +5,28 @@ library(haven)
 library(dplyr)
 library(ggplot2)
 
+# Read the SPSS data file into a dataframe
+subset_data <- read.csv("subset_data.csv")
+View(subset_data)
 
-# Länder im Datensatz extrahieren
-unique_countries <- unique(subset_data$cntry)
-
-# Anzahl der Länder zählen
-num_countries <- length(unique_countries)
-
-# Anzeigen der Anzahl
-print(num_countries)
-
-show(unique_countries)
-
-# Durchschnittliche Depression nach Land
+# Average Depression per Land
 depression_by_country <- subset_data %>%
   group_by(cntry) %>%
   summarise(avg_depression = mean(w4q47, na.rm = TRUE))
 
-# Balkendiagramm für durchschnittliche Depression
+# Bar Chart for average Depression
 ggplot(depression_by_country, aes(x = reorder(cntry, -avg_depression), y = avg_depression)) +
   geom_bar(stat = "identity", fill = "skyblue") +
   labs(x = "Land", y = "Durchschnittliche Depression", title = "Durchschnittliche Depression nach Ländern") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+# Average household income per country
+hincome_by_country <- subset_data %>%
+  group_by(cntry) %>%
+  summarise(avg_hincome = mean(hinctnta, na.rm = TRUE))
 
+# Bar chart for average household income
+ggplot(hincome_by_country, aes(x = reorder(cntry, -avg_hincome), y = avg_hincome)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(x = "Land", y = "Durchschnittliches Haushaltseinkommen", title = "Durchschnittliches Haushaltseinkommen nach Ländern") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
