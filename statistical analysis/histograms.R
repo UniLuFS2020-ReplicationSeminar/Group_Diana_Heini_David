bf926d712b4d07ccec0f818b7d766e9dc7987c87
Heini Järviö <heinimjarv@gmail.com>
2024-03-26 11:27:04 +0100

    Creating histograms for the gender and country differences

---
+++ statistical analysis/histograms.R
@@ -0,0 +1,28 @@
+# Average Depression per Land
+depression_by_country <- data_depression %>%
+  group_by(country) %>%
+  summarise(avg_depression = mean(depression, na.rm = TRUE))
+
+View(depression_by_country)
+
+# Bar Chart for average Depression
+ggplot(depression_by_country, aes(x = reorder(country, -avg_depression), y = avg_depression)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(x = "Land", y = "Durchschnittliche Depression", title = "Durchschnittliche Depression nach Ländern") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# Average Depression per gender
+depression_by_gender <- data_depression %>%
+  group_by(gender) %>%
+  summarise(avg_depression = mean(depression, na.rm = TRUE))
+
+View(depression_by_gender)
+
+#Bar chart for average Depression by gender
+
+ggplot(depression_by_gender, aes(x = reorder(gender, -avg_depression), y = avg_depression)) +
+  geom_bar(stat = "identity", fill = "skyblue") +
+  labs(x = "gender", y = "Durchschnittliche Depression", title = "Durchschnittliche Depression nach Geschlecht") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+#Based on this there seems to be a difference in depression between the genders and countries
