cd92381c9c8794a84f57af75d803256294c15fe7
Heini Järviö <heinimjarv@gmail.com>
2024-03-26 12:29:44 +0100

    Added new histogram for gender x country

--- statistical analysis/histograms.R
+++ statistical analysis/histograms.R
@@ -26,3 +26,31 @@ ggplot(depression_by_gender, aes(x = reorder(gender, -avg_depression), y = avg_d
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 #Based on this there seems to be a difference in depression between the genders and countries
+
+#Note; only 2 data points for gender = no answer -> should be left out from the analysis?
+
+#Calculating means in depression per gender & country
+
+gender_by_country <- data_depression %>%
+  group_by(country, gender) %>%
+  summarise(mean_variable = mean(depression))
+
+print(gender_by_country)
+
+#creating a data frame of the grouping
+
+result <- data.frame(gender_by_country)
+
+View(result)
+
+#creating a bar chart to visually compare the grouping
+
+ggplot(gender_by_country, aes(x = country, y = mean_variable, fill = gender)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Mean Variable by Country and Gender",
+       x = "Country",
+       y = "Mean Variable") +
+  theme_minimal()
+
+#looks like females have higher depression scores in all countries except Island and Italy
+                            
