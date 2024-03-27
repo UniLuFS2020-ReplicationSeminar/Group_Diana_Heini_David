5f7ad6add0bddc60991ebd5737a9f08e7118fdf4
Heini Järviö <heinimjarv@gmail.com>
2024-03-27 09:29:26 +0100

    Small changes

--- data_prep/subset_data.R
+++ data_prep/subset_data.R
@@ -11,7 +11,7 @@ data <- read.csv("CRON2W4e01.csv")
 
 View(data)
 
-#filtering a subset of columns in the data needed for the analysis: country, age, gender, depression variable, household income, region
+#filtering a subset of columns in the data that might be relevant for the analysis: country, age, gender, depression variable, household income, region
 
 data_depression <- data %>% select(cntry, agea, gndr, w4q47, hinctnta, region)
 
@@ -40,9 +40,11 @@ data_depression$gender[data_depression$gender == "9"] <- "No answer"
 
 #checking for missing values in the dataset
 
-# Replace missing values marked with 9999 and 999 with NA
+# Replace missing values marked with 9999 and 999 with NA and "no answer" in gender with NA
 data_depression[data_depression == 99999] <- NA
 data_depression[data_depression == 999] <- NA
+
+View(data_depression)
 
 missing_values <- data_depression %>%
   summarise_all(~sum(is.na(.)))
@@ -51,6 +53,4 @@ missing_values <- data_depression %>%
 print(missing_values)
 
 #country has 1 missing value, gender has 4
-
-
 
