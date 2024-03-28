30129bc33e858918328bb09d357b6dff0676a60e
Heini Järviö <heinimjarv@gmail.com>
2024-03-28 10:58:01 +0100

    fixed gender groupings

--- statistical analysis/histograms.R
+++ statistical analysis/histograms.R
@@ -29,33 +29,45 @@ ggplot(depression_by_gender, aes(x = reorder(gender, -avg_depression), y = avg_d
 
 #Note; only 2 data points for gender = no answer -> should be left out from the analysis?
 
-#Calculating means in depression per gender & country
+#creating a new column gender2 that includes only Female and Male values from the gender column
 
-gender_by_country <- data_depression %>%
-  group_by(country, gender) %>%
-  summarise(mean_variable = mean(depression))
+data_depression$gender2 <- NA
 
-print(gender_by_country)
+data_depression$gender2[data_depression$gender %in% c("Male", "Female")] <- data_depression$gender[data_depression$gender %in% c("Male", "Female")]
+
+#checking that it worked
+
+View(data_depression)
+
+#It did so we calculate the average depression per gender & country
+
+depression_by_gender2 <- data_depression %>%
+  group_by(gender2) %>%
+  summarise(avg_depression = mean(depression, na.rm = TRUE))
+
+gender2_by_country <- data_depression %>%
+  group_by(country, gender2) %>%
+  summarise(mean_variable = mean(depression), .groups = 'drop') %>%
+  na.omit()
+
+print(gender2_by_country)
 
 #creating a data frame of the grouping
 
-result <- data.frame(gender_by_country)
+result <- data.frame(gender2_by_country)
 
 View(result)
 
 #creating a bar chart to visually compare the grouping
 
-library(haven)
-library(dplyr)
-library(magrittr)
-library(ggplot2)
-
-ggplot(gender_by_country, aes(x = country, y = mean_variable, fill = gender)) +
+ggplot(gender2_by_country, aes(x = country, y = mean_variable, fill = gender2)) +
   geom_bar(stat = "identity", position = "dodge") +
-  labs(title = "Mean Variable by Country and Gender",
-       x = "Country",
-       y = "Mean Variable") +
-  theme_minimal()
+  labs(title = "Average depression by Country and Gender")
+       labs(title = "Average depression in different countries",
+            x = "Country",
+            y = "Mean Variable" +
+  theme_minimal())
 
 #looks like females have higher depression scores in all countries except Island and Italy
-                            
+#biggest differences seems to be in Austria, Belgium and Czech Republic
+
